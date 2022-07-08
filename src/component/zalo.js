ae88d6654255e1ab51774a7d0bde7961d665b8dc
Tuan Nguyen <ngoctuanit07@gmail.com>
2022-07-08 21:10:10 +0700

    nang cap thanh module

---
+++ src/component/zalo.js
@@ -0,0 +1,57 @@
+import React from "react";
+const Zalo = () => {
+    return (
+        <><h1>Nhóm làm việc Zalo</h1><div className="container">
+            <div className="row">
+                <div className="col-md-2 mt-2">
+                    <a
+                        href="https://zalo.me/g/bgyiui426"
+                        target="_blank"
+                        className="btn btn-primary"
+                        rel="noreferrer"
+                    >
+                        Thiết Kế Web Nhà Hàng
+                    </a>
+                </div>
+                <div className="col-md-2 mt-2">
+                    <a
+                        href="https://zalo.me/g/stgxkt703"
+                        target="_blank"
+                        className="btn btn-primary"
+                    >
+                        Nhóm Design của team
+                    </a>
+                </div>
+                <div className="col-md-2 mt-2">
+                    <a
+                        href="https://zalo.me/g/lnsxcl891"
+                        target="_blank"
+                        className="btn btn-primary"
+                    >
+                        Thiết kế nội thất phú an
+                    </a>
+                </div>
+                <div className="col-md-2 mt-2">
+                    <a
+                        href="https://zalo.me/g/gjgxqj821"
+                        target="_blank"
+                        className="btn btn-primary"
+                    >
+                        Thiết kế dịch vụ{" "}
+                    </a>
+                </div>
+                <div className="col-md-2 mt-2">
+                    <a
+                        href="https://zalo.me/g/bxhnnr491"
+                        target="_blank"
+                        className="btn btn-primary"
+                    >
+                        Server{" "}
+                    </a>
+                </div>
+            </div>
+        </div></>
+    );
+
+};
+export default Zalo;
