d111f2f5d636cd2de89a5833bfb3a3908232392d
Tuan Nguyen <ngoctuanit07@gmail.com>
2022-07-08 14:11:51 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Nhóm làm việc Zalo</h1>
+      <div class="container">
+        <div className="row">
+          <div className="col-md-2 mt-2">
+            <a
+              href="https://zalo.me/g/bgyiui426"
+              target="_blank"
+              class="btn btn-primary"
+              rel="noreferrer"
+            >
+              Thiết Kế Web Nhà Hàng
+            </a>
+          </div>
+          <div className="col-md-2 mt-2">
+            <a
+              href="https://zalo.me/g/stgxkt703"
+              target="_blank"
+              class="btn btn-primary"
+            >
+              Nhóm Design của team
+            </a>
+          </div>
+          <div className="col-md-2 mt-2">
+            <a
+              href="https://zalo.me/g/lnsxcl891"
+              target="_blank"
+              class="btn btn-primary"
+            >
+              Thiết kế nội thất phú an
+            </a>
+          </div>
+          <div className="col-md-2 mt-2">
+            <a
+              href="https://zalo.me/g/gjgxqj821"
+              target="_blank"
+              class="btn btn-primary"
+            >
+              Thiết kế dịch vụ{" "}
+            </a>
+          </div>
+          <div className="col-md-2 mt-2">
+            <a
+              href="https://zalo.me/g/bxhnnr491"
+              target="_blank"
+              class="btn btn-primary"
+            >
+              Server{" "}
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
